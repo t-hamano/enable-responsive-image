7b1265711aff4cbe7401cbd375069c0cf3b265e0
Aki Hamano <tetsuaki.hamano@gmail.com>
2025-03-17 13:29:11 +0900

    Stylelint: Use Stylistic

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-	extends: [ '@wordpress/stylelint-config/scss' ],
+	extends: [ '@wordpress/stylelint-config/scss-stylistic' ],
 	rules: {
 		'no-descending-specificity': null,
 		'font-weight-notation': null,
